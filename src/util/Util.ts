a0f23d4ee375b6e5fce8c1c51cded9f78d2cbdd1
Santosh Bhandari <contact@santoshb.com.np>
2021-10-17 15:29:50 +0545

    refactor: Remove class instance from utils

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,12 +1,12 @@
-export default class Util {
-	public encodeTag(tag: string) {
-		return encodeURIComponent(this.parseTag(tag));
-	}
-
-	public parseTag(tag: string) {
-		if (tag && typeof tag === 'string') {
-			return `#${tag.toUpperCase().replace(/O|o/g, '0').replace(/^#/g, '')}`;
-		}
-		throw TypeError('The "tag" argument must be of type string.');
-	}
+export function encodeTag(tag: string) {
+	return encodeURIComponent(parseTag(tag));
 }
+
+export function parseTag(tag: string) {
+	return `#${tag.toUpperCase().replace(/O|o/g, '0').replace(/^#/g, '')}`;
+}
+
+export default {
+	encodeTag,
+	parseTag
+};
