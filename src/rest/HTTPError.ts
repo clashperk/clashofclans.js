ff22f352f51e8cdb64a66b15874f4b979fc0373d
Suvajit <suvajit.me@gmail.com>
2021-11-18 22:04:47 +0530

    docs: update type docs

--- src/rest/HTTPError.ts
+++ src/rest/HTTPError.ts
@@ -1,11 +1,21 @@
 const messages: { [key: string]: string } = {
+	500: 'Unknown error happened when handling the request.',
 	504: 'The user aborted a request.',
-	404: 'Resource was not found.'
+	404: 'Resource was not found.',
+	400: 'Client provided incorrect parameters for the request.',
+	503: 'Service is temporarily unavailable because of maintenance.',
+	429: 'Request was throttled, because amount of requests was above the threshold defined for the used API token.',
+	403: 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.'
 };
 
 const reasons: { [key: string]: string } = {
 	504: 'networkTimeout',
-	404: 'notFound'
+	404: 'notFound',
+	503: 'serviceUnavailable',
+	429: 'tooManyRequests',
+	400: 'badRequest',
+	403: 'forbidden',
+	500: 'unknownError'
 };
 
 /** Represents an HTTP Error. */
