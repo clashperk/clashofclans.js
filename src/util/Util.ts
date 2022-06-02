4b74c2aeb3e78b565442b9e3d96eea7b42ca643c
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-06-02 20:38:35 +0530

    feat: add parseArmyLink

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -128,4 +128,30 @@ export class Util extends null {
 	public static async delay(ms: number) {
 		return new Promise((res) => setTimeout(res, ms));
 	}
+
+	/** Parse In game army link into troops and spells into count with their id's*/
+	public static parseArmyLink(link: string) {
+		const units: { troopId: number; count: number }[] = [];
+		const spells: { spellId: number; count: number }[] = [];
+
+		const matches = link.matchAll(/u(?<units>[\d+x-]+)|s(?<spells>[\d+x-]+)/g);
+
+		for (const match of matches) {
+			if (match.groups?.units) {
+				for (const unit of match.groups.units.split('-')) {
+					const [count, troopId] = unit.split('x').map(Number);
+					units.push({ count, troopId });
+				}
+			}
+
+			if (match.groups?.spells) {
+				for (const spell of match.groups.spells.split('-')) {
+					const [count, spellId] = spell.split('x').map(Number);
+					spells.push({ count, spellId });
+				}
+			}
+		}
+
+		return { units, spells };
+	}
 }
